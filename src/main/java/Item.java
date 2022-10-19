eef4803c13d45e9483143a7249ddd5f9082269d7
Brian Dsouza <briandsouza9820@gmail.com>
2022-10-19 23:53:35 +0530

    Final Solution

---
+++ src/main/java/Item.java
@@ -0,0 +1,140 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Item implements List<Item> {
+    private String name;
+    private int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+    @Override
+    public String toString(){
+        return  name + ":"
+                + price
+                + "\n"
+                ;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(Item item) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Item> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends Item> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Item get(int index) {
+        return null;
+    }
+
+    @Override
+    public Item set(int index, Item element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Item element) {
+
+    }
+
+    @Override
+    public Item remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<Item> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<Item> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<Item> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
